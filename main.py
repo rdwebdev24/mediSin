398fa60b55c1a76e2c5d70dad63f454b586f63fd
Tanmay Tripathy <ttripathy17@gmail.com>
2022-08-20 20:48:50 +0530

    isolated backend

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
+from base64 import encode
 import pickle
 from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from starlette.responses import JSONResponse
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+loaded_model = pickle.load(open('model.h5', 'rb'))
+
+DATA_PATH = "./Training.csv"
+data = pd.read_csv(DATA_PATH).dropna(axis = 1)
+X = data.iloc[:,:-1]
+y = data.iloc[:,-1]
+
+symptoms = X.columns.values
+
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+
+data_dict = {
+    "symptom_index":symptom_index,
+    "predictions_classes":y
+}
+
+def predictDisease(symptoms):
+    symptoms = symptoms.split(",")
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"][symptom]
+        input_data[index] = 1
+         
+    input_data = np.array(input_data).reshape(1,-1)
+    svm_prediction = data_dict["predictions_classes"][loaded_model.predict(input_data)[0]] 
+    return svm_prediction
+ 
 
 app = FastAPI()
 
@@ -22,11 +60,11 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return JSONResponse(status_code=200,content = {"Server":"Up"}) 
 
 @app.get("/run")
 def read_item(q: Union[str, None] = None):
-    loaded_model = pickle.load(open('model.h5', 'rb'))
-    predict = loaded_model.predict(q)
-    return {'res' : predict}
+    predict = predictDisease(q)
+    body = {'res' : predict}
+    return JSONResponse(status_code=200,content = body)
+    
