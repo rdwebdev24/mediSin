a84aebacacb62f4b10201e4a5b1e6baf5cc45f37
Tanmay Tripathy <ttripathy17@gmail.com>
2022-08-20 19:33:13 +0530

    added CORS

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pickle
 from typing import Union
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
